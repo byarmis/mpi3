ab57acb3143724825ee1adeb9fbe7eeb537785b9
Ben Yarmis <ben@yarm.is>
2019-07-17 09:06:38 -0500

    Moved Statement out.  Should be in a model-specific file / location, but that's for later.

--- mpi3/model/db.py
+++ mpi3/model/db.py
@@ -4,23 +4,23 @@
 import logging
 import os
 import re
-import sqlite3
 import string
-from typing import Tuple, Iterable, List, Dict, Callable, Set
-from functools import lru_cache
-
 from mutagen.easyid3 import EasyID3
 from mutagen.mp3 import MP3
+import sqlite3
+from typing import Iterable, List, Dict, Callable, Tuple
+from functools import lru_cache
 
+from mpi3.types import Statement
 from mpi3.model.SQL import (
     CREATE_STATEMENTS,
     GET_BY_ID,
     GET_COUNT,
     GET_PLAYLIST,
-    INSERT_SONGS,
+    TRUNCATE_TABLE,
 )
 from mpi3.model.menu_items import SongButton
-from mpi3.model.types import Filter
+from mpi3.types import Filter
 
 logger = logging.getLogger(__name__)
 
@@ -46,147 +46,6 @@ class OpenConnection:
         self.conn.close()
 
 
-class BatchAdder:
-    def __init__(self, config: Dict) -> None:
-        logger.info('Initializing BatchAdder')
-        # _buffer is a list of tuples
-        self._buffer = []
-        self.buffer_size = config['buffer_load_size']
-        self.db_file = config['library']
-
-        # self.stop_chars = {c: None for c in string.punctuation + string.digits}
-        self.stop_chars = {c: None for c in string.punctuation}  # + string.digits}
-
-        if config.get('bad_tracks') is not None:
-            self.bad_tracks = {int(i) for i in config.get('bad_tracks', [])}
-        else:
-            self.bad_tracks = set()
-
-        if self.buffer_size > 500:
-            logger.warning('A buffer load size of {} was selected, '
-                           'but the max is 500'.format(self.buffer_size))
-            self.buffer_size = 500
-
-    def add(self, item: str, force_push: bool = False) -> None:
-        tags = self._extract_tags(item)
-        self._buffer.append(tags)
-
-        if len(self._buffer) >= self.buffer_size or force_push:
-            self._add_buffer()
-
-    def _extract_tags(self, f: str) -> Tuple:
-        logger.debug('Extracting tags for {}'.format(f))
-        audio = MP3(f, ID3=EasyID3)
-
-        title = audio.get('title')
-        album = audio.get('album')
-        artist = audio.get('artist')
-        track_tuple = audio.get('tracknumber')
-
-        if title:
-            title = title[0].strip()
-            sortable_title = title.lower().translate(self.stop_chars)
-            if not sortable_title:
-                # We removed too much!  Put it back?
-                sortable_title = title
-        else:
-            title = f.split('/')[-1]
-            sortable_title = None
-
-        album = album[0].strip() if album else None
-        artist = artist[0].strip() if artist else None
-
-        if track_tuple:
-            track_split = track_tuple[0].split('/')
-            track_number = int(track_split[0])
-            total_tracks = int(track_split[1]) if len(track_split) > 1 else None
-        else:
-            track_number, total_tracks = None, None
-
-        if track_number is None or track_number in self.bad_tracks:
-            # If it starts with ##-, set ## to track number
-            m = re.match(r'\A(\d*)\W', os.path.basename(f))
-            if m:
-                logger.info('Overwriting the current track number '
-                            '({}) with an auto-extracted one ({})'.format(track_number,
-                                                                          int(m.group(1))))
-                track_number = int(m.group(1))
-
-        t = (f, int(audio.info.length),
-             title, sortable_title, album, artist, track_number, total_tracks)
-
-        return t
-
-    def _add_buffer(self) -> None:
-        if not self._buffer:
-            return
-
-        logger.debug('Adding batch of songs ({})'.format(len(self._buffer)))
-        with OpenConnection(self.db_file) as db:
-            db.executemany(INSERT_SONGS, self._buffer)
-
-        self._buffer = []
-        logger.debug('\t... complete')
-
-
-class Statement(str):
-    """
-    A Statement is a string that can be built up progressively and interacted with like an object.
-
-    >>> s = Statement('SELECT * FROM {table}')
-    >>> str(s)
-    ValueError
-
-    >>> s.table = 'library'
-    >>> str(s)
-    'SELECT * FROM library'
-
-    A Statement also accept a dictionary in the constructor to pre-populate attributes
-    >>> s = Statement('SELECT * FROM {table} WHERE {where_clause}', {'table':'library'})
-    >>> str(s)
-    ValueError
-
-    >>> s.where_clause = '1 = 1'
-    >>> str(s)
-    'SELECT * FROM library WHERE 1 = 1'
-    """
-
-    def __new__(cls, a, *args, **kw):
-        return str.__new__(cls, a)
-
-    regex = r'.*{(.*)}.*'
-
-    def __init__(self, sql_statement: str, requirements: Dict[str, str] = None) -> None:
-        super().__init__()
-        self.sql = sql_statement
-        self.required = self._process_required(self.sql)
-
-        self._expected = {'sql', 'required'}
-
-        if requirements is not None:
-            for requirement, value in requirements.items():
-                setattr(self, requirement, value)
-
-    def __str__(self) -> str:
-        missing_requirements = self.required - set(self.__dict__.keys())
-        if missing_requirements:
-            raise ValueError(f'Required statement(s) not defined: {",".join(missing_requirements)}')
-
-        return self.sql.format(**self.__dict__)
-
-    def __repr__(self) -> str:
-        kwargs = {k: v for k, v in self.__dict__.items() if not k.startswith('_') and k not in self._expected}
-        return f'Statement({self.sql}, {kwargs})'
-
-    def _process_required(self, sql: str) -> Set[str]:
-        match = re.findall(self.regex, sql, flags=re.MULTILINE)
-
-        if match is None:
-            return set()
-        else:
-            return set(match)
-
-
 class Database:
     def __init__(self, config: Dict) -> None:
         logger.info('Initializing Database')
@@ -194,6 +53,7 @@ class Database:
         self.db_file = os.path.expanduser(config['library'])
         self.adder = BatchAdder(config)
 
+        self.TABLES = set()
         self.create_db()
         self.scan_libraries()
 
@@ -204,9 +64,10 @@ class Database:
         else:
             logger.info('Library file ({}) does not exist and will be created'.format(self.db_file))
 
-        for statement in CREATE_STATEMENTS:
+        for table, statement in CREATE_STATEMENTS:
             with OpenConnection(self.db_file) as db:
                 db.execute(statement)
+            self.TABLES.add(table)
 
     def scan_libraries(self) -> None:
         logger.info('Scanning following directory(s):{}'.format('\n\t'.join(self.dirs)))
@@ -228,10 +89,10 @@ class Database:
         self.adder.add(force_push=True)
 
     @lru_cache()
-    def get_count(self, filters: Filter = None) -> int:
+    def get_count(self, table: str = 'library', filters: Filter = None) -> int:
         with OpenConnection(self.db_file) as db:
             filter_statement = self._get_filters(filters)
-            c = db.execute(GET_COUNT.format(filter_statement=filter_statement)).fetchall()[0][0]
+            c = db.execute(GET_COUNT.format(table=table, filter_statement=filter_statement)).fetchall()[0][0]
 
         return c
 
@@ -292,7 +153,7 @@ class Database:
                            play_song=play_func) for ID in ids]
 
     @staticmethod
-    def _get_filters(filters: Dict[str, List[str]]) -> str:
+    def _get_filters(filters: Filter) -> str:
         # {'artist': ['a'], 'album':['b','c']}
         filter_str = ''
         if filters:
@@ -307,3 +168,94 @@ class Database:
     def run(self, query: Statement) -> None:
         with OpenConnection(self.db_file) as db:
             db.execute(query)
+
+    def truncate(self, table: str) -> None:
+        if table not in self.TABLES:
+            raise ValueError(f'{table} does not exist to TRUNCATE')
+
+        with OpenConnection(self.db_file) as db:
+            db.execute(TRUNCATE_TABLE.format(table=table))
+
+
+class BatchAdder:
+    def __init__(self, config: Dict) -> None:
+        logger.info('Initializing BatchAdder')
+        # _buffer is a list of tuples
+        self._buffer = []
+        self.buffer_size = config['buffer_load_size']
+        self.db_file = config['library']
+
+        # self.stop_chars = {c: None for c in string.punctuation + string.digits}
+        self.stop_chars = {c: None for c in string.punctuation}  # + string.digits}
+
+        if config.get('bad_tracks') is not None:
+            self.bad_tracks = {int(i) for i in config.get('bad_tracks', [])}
+        else:
+            self.bad_tracks = set()
+
+        if self.buffer_size > 500:
+            logger.warning('A buffer load size of {} was selected, '
+                           'but the max is 500'.format(self.buffer_size))
+            self.buffer_size = 500
+
+    def add(self, item: str = None, force_push: bool = False) -> None:
+        if item is not None:
+            tags = self._extract_tags(item)
+            self._buffer.append(tags)
+
+        if len(self._buffer) >= self.buffer_size or force_push:
+            self._add_buffer()
+
+    def _extract_tags(self, f: str) -> Tuple:
+        logger.debug('Extracting tags for {}'.format(f))
+        audio = MP3(f, ID3=EasyID3)
+
+        title = audio.get('title')
+        album = audio.get('album')
+        artist = audio.get('artist')
+        track_tuple = audio.get('tracknumber')
+
+        if title:
+            title = title[0].strip()
+            sortable_title = title.lower().translate(self.stop_chars)
+            if not sortable_title:
+                # We removed too much!  Put it back?
+                sortable_title = title
+        else:
+            title = f.split('/')[-1]
+            sortable_title = None
+
+        album = album[0].strip() if album else None
+        artist = artist[0].strip() if artist else None
+
+        if track_tuple:
+            track_split = track_tuple[0].split('/')
+            track_number = int(track_split[0])
+            total_tracks = int(track_split[1]) if len(track_split) > 1 else None
+        else:
+            track_number, total_tracks = None, None
+
+        if track_number is None or track_number in self.bad_tracks:
+            # If it starts with ##-, set ## to track number
+            m = re.match(r'\A(\d*)\W', os.path.basename(f))
+            if m:
+                logger.info('Overwriting the current track number '
+                            '({}) with an auto-extracted one ({})'.format(track_number,
+                                                                          int(m.group(1))))
+                track_number = int(m.group(1))
+
+        t = (f, int(audio.info.length),
+             title, sortable_title, album, artist, track_number, total_tracks)
+
+        return t
+
+    def _add_buffer(self) -> None:
+        if not self._buffer:
+            return
+
+        logger.debug('Adding batch of songs ({})'.format(len(self._buffer)))
+        with OpenConnection(self.db_file) as db:
+            db.executemany(INSERT_SONGS, self._buffer)
+
+        self._buffer = []
+        logger.debug('\t... complete')
