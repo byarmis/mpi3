9404e1f85c0eb9bf209d9cc167a1446c90be888f
Ben Yarmis <ben@yarm.is>
2020-02-18 19:20:19 -0600

    Done?

--- mpi3/static/shell_scripts/_update.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo docker pull byarmis/mpi3:latest
-
-sudo reboot
-
--- mpi3/static/shell_scripts/update.sh
+++ mpi3/static/shell_scripts/update.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
 
-papirus-write "Updating Docker Image" --rotation 90
+papirus-write "Updating: Linux" --rotation 90
+sudo apt-get update
+papirus-write "Updating: Linux Install" --rotation 90
+sudo apt-get upgrade -y
+papirus-write "Updating: Linux Autoremove" --rotation 90 --fsize 15
+sudo apt-get autoremove -y
 
-echo _update >> /docker-communication
+papirus-write "Updating: Python Packages" --rotation 90
+pip3 install -r requirements.txt --upgrade --user
+
+papirus-write "Updating: PaPiRus" --rotation 90
+cd $HOME_DIR_papirus
+git checkout master
+git pull origin master
+sudo python3 setup.py install
+
+papirus-write "Update complete" --rotation 90
+sleep 3
+
+$HOME_DIR_mpi3/mpi3/static/shell_scripts/reboot.sh
 
